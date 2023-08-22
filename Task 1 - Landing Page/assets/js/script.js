f7b5475e25064d8a57333e41dd23b12826b7e4e4
VcodeGen <142864121+VcodeGen@users.noreply.github.com>
2023-08-22 01:58:37 -0700

    Add files via upload

---
+++ Task 1 - Landing Page/assets/js/script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+const header = document.querySelector("[data-header]");
+
+navToggleBtn.addEventListener("click", function () {
+  header.classList.toggle("active");
+});
